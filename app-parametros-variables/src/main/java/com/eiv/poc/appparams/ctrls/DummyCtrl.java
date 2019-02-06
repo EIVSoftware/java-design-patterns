0556f99893a5ee775d08ae99d101bcc17ef40325
Diego Cairone <diegocairone@gmail.com>
2019-02-06 12:06:23 -0300

    Midiendo el tiempo con HashSet en lugar de Hashmap

--- app-parametros-variables/src/main/java/com/eiv/poc/appparams/ctrls/DummyCtrl.java
+++ app-parametros-variables/src/main/java/com/eiv/poc/appparams/ctrls/DummyCtrl.java
@@ -1,5 +1,7 @@
 package com.eiv.poc.appparams.ctrls;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,14 +13,21 @@ import com.eiv.poc.appparams.enums.ParametroEnumImpl;
 @RestController
 public class DummyCtrl {
 	
+	private static final Logger LOG = LoggerFactory.getLogger(DummyCtrl.class);
+	
 	@Autowired private ParametrosStoreGlobalScope parametrosStoreGlobalScope = null;
 	
 	@GetMapping("/api/entidades/{entidadId}/parametrosuno")
 	public String entidadValorParametroUno(@PathVariable("entidadId") Long entidadId) {
 
+		Long start = System.nanoTime();
+		
 		Integer valor = parametrosStoreGlobalScope
 				.filterByEntidadId(entidadId)
 				.getParametroValor(ParametroEnumImpl.PARAMETRO_UNO);
+		
+		Long end = System.nanoTime();
+		LOG.info("Enlapsed: {}", end - start);
 		
 		return valor.toString();
 	}
