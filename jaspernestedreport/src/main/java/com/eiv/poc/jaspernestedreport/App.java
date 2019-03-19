4aabe91633e6b4963e567fc0d9d5a6b4f074f58b
diego.cairone <diego.cairone@EQ016.eivsoftware.net>
2019-03-19 15:46:29 -0300

    Se agrega tabla de direcciones

--- jaspernestedreport/src/main/java/com/eiv/poc/jaspernestedreport/App.java
+++ jaspernestedreport/src/main/java/com/eiv/poc/jaspernestedreport/App.java
@@ -1,13 +1,7 @@
 package com.eiv.poc.jaspernestedreport;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
     }
 }
--- jaspernestedreport/src/main/java/com/eiv/poc/jaspernestedreport/PersonaBeanFactory.java
+++ jaspernestedreport/src/main/java/com/eiv/poc/jaspernestedreport/PersonaBeanFactory.java
@@ -10,9 +10,9 @@ public class PersonaBeanFactory {
         Collection<PersonaBeanImpl> personas = Arrays.asList(
                 new PersonaBeanImpl(1L, "HOMERO SIMPSON", 
                         Arrays.asList(
-                                new PersonaDireccionBeanImpl("Evergreen Terrace", "742"),
-                                new PersonaDireccionBeanImpl("Evergreen Terrace", "742"),
-                                new PersonaDireccionBeanImpl("Evergreen Terrace", "742"))
+                                new PersonaDireccionBeanImpl("Evergreen Terrace 1", "742"),
+                                new PersonaDireccionBeanImpl("Evergreen Terrace 2", "742"),
+                                new PersonaDireccionBeanImpl("Evergreen Terrace 3", "742"))
                         ),
                 new PersonaBeanImpl(2L, "MARGE SIMPSON"),
                 new PersonaBeanImpl(3L, "BART SIMPSON"),
