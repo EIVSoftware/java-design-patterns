109b1aa88cdd2e24faa80f54294acfdd07fdbdec
Diego Cairone <dcairone@eiva.com.ar>
2019-03-29 16:09:37 -0300

    Get refresh token

--- oauth2-api-web/src/main/java/com/eiv/poc/apiweb/ctrls/HomeCtrl.java
+++ oauth2-api-web/src/main/java/com/eiv/poc/apiweb/ctrls/HomeCtrl.java
@@ -1,13 +1,23 @@
 package com.eiv.poc.apiweb.ctrls;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeCtrl {
 
+    private Logger LOG = LoggerFactory.getLogger(HomeCtrl.class);
+    
     @GetMapping("/home")
-    public String home() {
+    public String home(
+            @RegisteredOAuth2AuthorizedClient("eivas") OAuth2AuthorizedClient authorizedClient) {
+        
+        LOG.debug("Refresh token: {}", 
+                authorizedClient.getRefreshToken().getTokenValue());
         return "HOME!!";
     }
 }
