09ae46c55dec1fdef41e53a1e97d5080a4e22fd7
Diego Cairone <diegocairone@gmail.com>
2021-06-30 14:21:18 -0300

    Use case: persona desde direccion principal y direcciones alternativas

--- mapstruct-poc01/src/main/java/com/eiv/view/mapper/ContactMapper.java
+++ mapstruct-poc01/src/main/java/com/eiv/view/mapper/ContactMapper.java
@@ -4,6 +4,8 @@ import com.eiv.data.model.AddressEntity;
 import com.eiv.data.model.PersonEntity;
 import com.eiv.view.model.ContactModel;
 
+import java.util.List;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
@@ -21,5 +23,13 @@ public interface ContactMapper {
     @Mapping(target = "mainAddress", source = "addressEntity")
     @Mapping(target = "altAddresses", ignore = true)
     ContactModel fromAddressEntity(AddressEntity addressEntity);
+    
+    @Mapping(target = "mainAddress", source = "addressEntity")
+    @Mapping(target = "altAddresses", source = "addressEntities")
+    @Mapping(target = "names", source = "addressEntity.person.names")
+    @Mapping(target = "birthDate", source = "addressEntity.person.birthDate")
+    @Mapping(target = "gender", source = "addressEntity.person.gender")
+    @Mapping(target = "id", source = "addressEntity.person.id")
+    ContactModel fromAddressesEntity(AddressEntity addressEntity, List<AddressEntity> addressEntities);
 
 }
