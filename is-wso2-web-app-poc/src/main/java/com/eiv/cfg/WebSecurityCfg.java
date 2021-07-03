24b4113387d11e7ba78654bac369d95e72da62a7
diego.cairone <dcairone@eiva.com.ar>
2021-07-03 14:57:56 -0300

    Now authenticating through WSO2 IS

---
+++ is-wso2-web-app-poc/src/main/java/com/eiv/cfg/WebSecurityCfg.java
@@ -0,0 +1,48 @@
+package com.eiv.cfg;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+@EnableWebSecurity
+public class WebSecurityCfg extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        
+        http
+            .csrf()
+                .disable()
+            .authorizeRequests()
+                .antMatchers("/login")
+                    .permitAll()
+                .anyRequest()
+                    .authenticated()
+            .and()
+            .oauth2Login()
+                .loginPage("/login")
+            .and()
+            .logout()
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+                .logoutSuccessHandler(oidcLogoutSuccessHandler());
+        
+    }
+
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
+
+    private LogoutSuccessHandler oidcLogoutSuccessHandler() {
+        
+        OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler =
+                new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+
+        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("http://localhost:8080");
+
+        return oidcLogoutSuccessHandler;
+    }
+}
---
+++ is-wso2-web-app-poc/src/main/java/com/eiv/ui/ctrl/LoginCtrl.java
@@ -0,0 +1,19 @@
+package com.eiv.ui.ctrl;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class LoginCtrl {
+
+    @GetMapping("/login")
+    public String loginFrm() {
+        return "login";
+    }
+    
+    @PostMapping("/login")
+    public String loginPost() {
+        return "redirect:/oauth2/authorization/wso2";
+    }
+}
